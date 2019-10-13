3ea31df314546d752bb841ba1d41856f8c685a54
CCode02 <cristiancameloariza@gmail.com>
2019-10-13 15:29:26 -0500

    guiigu

---
+++ api/models/messagesHistories.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const MessageHistoryModel = new Schema({
+    description: String,
+    typeMessage: String,
+    date: String,
+    goals: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'goal'
+    }
+});
+
+module.exports = mongoose.model('MessageHistory', MessageHistoryModel);
