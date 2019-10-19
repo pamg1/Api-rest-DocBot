4866c6e429384bcb1feebf970f172eddef2f087f
pamg1 <pamg0107@gmail.com>
2019-10-19 15:35:24 -0500

    Put en paciente

--- api/routes/patients.js
+++ api/routes/patients.js
@@ -6,7 +6,7 @@ const controller = require("../controllers/patients");
 router.route("/")
     .get(controller.all)
     .post(controller.post)
-    .put(controller.validate,controller.put);
+    .put(controller.put);
 
 router.route("/buscar")
     .post(controller.findpatients);    
