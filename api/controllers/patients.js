d2a9871d49565d67e16bf08d6db83591388192de
pamg1 <pamg0107@gmail.com>
2019-10-23 03:17:31 -0500

    Cambio

--- api/controllers/patients.js
+++ api/controllers/patients.js
@@ -110,7 +110,7 @@ exports.putweight = (req, res, next) => {
     const updates = req.body;
     const id = updates["id"];
     console.log(updates);
-    Patient.updateOne({ '_id': id }, {$push:{'weight':{'value': updates["value"]}}}, function (err, patient) {
+    Patient.updateOne({ '_id': id }, {$push:{'weight':{'value': updates["weight"]}}}, function (err, patient) {
         if(err){
             console.log(err);
         }
