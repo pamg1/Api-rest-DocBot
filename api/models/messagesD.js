e8919f8395dc7efbf8452b2c258f0b60891b35c2
pamg1 <pamg0107@gmail.com>
2019-10-29 23:18:13 -0500

    Cambio

--- api/models/messagesD.js
+++ api/models/messagesD.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const MessageDModel = new Schema({
+    subject: String,
     description: String,
     date: String,
     patient: { 
