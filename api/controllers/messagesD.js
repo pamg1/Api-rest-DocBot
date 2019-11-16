f5183f7dcbc82ae027ca83848266944b5d79cf48
pamg1 <pamg0107@gmail.com>
2019-11-16 18:27:08 -0500

    notification test 1

--- api/controllers/messagesD.js
+++ api/controllers/messagesD.js
@@ -33,33 +33,32 @@ exports.post = (req, res, next) => {
     // Create the messages that you want to send to clents
     let pushToken = patient;
     let messages = [];
-        // Each push token looks like ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]
-        // Check that all your push tokens appear to be valid Expo push tokens
-        if (!Expo.isExpoPushToken(pushToken)) {
-            console.error(`Push token ${pushToken} is not a valid Expo push token`);
-            continue;
-        }
+    // Each push token looks like ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]
+    // Check that all your push tokens appear to be valid Expo push tokens
+    if (!Expo.isExpoPushToken(pushToken)) {
+        console.error(`Push token ${pushToken} is not a valid Expo push token`);
+        continue;
+    }
 
-        // Construct a message (see https://docs.expo.io/versions/latest/guides/push-notifications.html)
-        messages.push({
-        to: pushToken,
-        sound: 'default',
-        body: 'Que rico too eso :v',
-        data: { withSome: 'data' },
-        }) 
+    // Construct a message (see https://docs.expo.io/versions/latest/guides/push-notifications.html)
+    messages.push({
+    to: pushToken,
+    sound: 'default',
+    body: 'Que rico too eso :v',
+    data: { withSome: 'data' },
+    });
     
     // The Expo push notification service accepts batches of notifications so
     // that you don't need to send 1000 requests to send 1000 notifications. We
     // recommend you batch your notifications to reduce the number of requests
     // and to compress them (notifications with similar content will get
     // compressed).
-    let chunks = expo.chunkPushNotifications(messages);
+    let chunk = expo.chunkPushNotifications(messages);
     let tickets = [];
     (async () => {
     // Send the chunks to the Expo push notification service. There are
     // different strategies you could use. A simple one is to send one chunk at a
     // time, which nicely spreads the load out over time:
-    for (let chunk of chunks) {
         try {
         let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
         console.log(ticketChunk);
@@ -71,7 +70,6 @@ exports.post = (req, res, next) => {
         } catch (error) {
         console.error(error);
         }
-    }
     })();
     // Later, after the Expo push notification service has delivered the
     // notifications to Apple or Google (usually quickly, but allow the the service
