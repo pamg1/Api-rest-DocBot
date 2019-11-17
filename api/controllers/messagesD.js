c85afa0897400966d7bbc7e19e05869f7f758ffd
pamg1 <pamg0107@gmail.com>
2019-11-16 19:34:51 -0500

    notification test 1

--- api/controllers/messagesD.js
+++ api/controllers/messagesD.js
@@ -29,7 +29,7 @@ exports.post = (req, res, next) => {
     });
     // Create a new Expo SDK client
     let expo = new Expo();
-
+    Console.log(patient);
     // Create the messages that you want to send to clents
     let pushToken = patient;
     let messages = [];
